ef316f683e2a1668c2c53bcd57c8f4692603ab0f
IHateComputer1 <114846652+IHateComputer1@users.noreply.github.com>
2022-10-14 09:24:52 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,54 +1,46 @@
 let count_down = 0
 input.onButtonPressed(Button.A, function () {
-    count_down = 1
-    for (let index = 0; index < 6; index++) {
-        basic.showNumber(count_down)
-        basic.pause(1000)
-        count_down += 1
-    }
+    count_down = -1
 })
 input.onButtonPressed(Button.B, function () {
     count_down = 9
-    for (let index = 0; index < 10; index++) {
+    while (count_down >= 0) {
         basic.showNumber(count_down)
-        basic.pause(1000)
         count_down += -1
     }
-    if (count_down == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # . # .
-            . . # . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . . . #
-            . # . # .
-            . . # . .
-            `)
-        basic.showLeds(`
-            . # . # .
-            # . . . #
-            . . . . .
-            # . . . #
-            . # . # .
-            `)
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
 })
